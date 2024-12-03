bac4f27249d211f28f9165e0ea47675758e26ccb
Rafael <tauschek.rafael@gmail.com>
2024-12-03 16:56:35 +0100

    make jwt and validate

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,13 @@
 package auth
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"net/http"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func HashPassword(password string) (string, error) {
 
@@ -20,4 +27,46 @@ func CheckPasswordHash(password, hash string) error {
 	}
 
 	return nil
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	time := time.Now().UTC()
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    "chirpy",
+		IssuedAt:  jwt.NewNumericDate(time),
+		ExpiresAt: jwt.NewNumericDate(time.Add(expiresIn)),
+		Subject:   userID.String(),
+	})
+
+	signedToken, err := token.SignedString(tokenSecret)
+	if err != nil {
+		return "", err
+	}
+
+	return signedToken, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+
+	token, err := jwt.ParseWithClaims(tokenString, jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(tokenSecret), nil
+	})
+
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	subject, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.Nil, err
+	}
+	uuid := uuid.MustParse(subject)
+
+	return uuid, nil
+}
+
+func GetBearerToken(headers http.Header) (string, error) {
+
+	return "", nil
 }
